2570def23af305d1642cd0ce7e323c2440fce94a
jacksparrow414 <33389378+jacksparrow414@users.noreply.github.com>
2020-05-17 12:22:33 +0800

    feat(schoolService.xml、userService.xml): add code

--- src/main/java/com/learn/spring/spring01/service/SchoolServiceImpl.java
+++ src/main/java/com/learn/spring/spring01/service/SchoolServiceImpl.java
@@ -1,5 +1,7 @@
 package com.learn.spring.spring01.service;
 
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -7,6 +9,7 @@ import java.time.format.DateTimeFormatter;
  * @author duhongbo
  * @date 2020/5/16 10:04
  */
+@Component
 public class SchoolServiceImpl {
 
     public String getTimeStr(){
