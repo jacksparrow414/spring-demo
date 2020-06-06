91baa06f546b5a5e0dd2aaa9a85dfd7dbdbd460c
duhongbo <duhb@youxuepai.com>
2020-06-06 15:26:54 +0800

    style使用lombok注解

--- src/main/java/com/learn/spring/spring01/entity/User.java
+++ src/main/java/com/learn/spring/spring01/entity/User.java
@@ -1,40 +1,19 @@
 package com.learn.spring.spring01.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * @author duhongbo
  * @date 2020/5/16 9:29
  */
+@Getter
+@Setter
+@ToString
 public class User {
 
     private Integer id;
     private String name;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public User(Integer id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
