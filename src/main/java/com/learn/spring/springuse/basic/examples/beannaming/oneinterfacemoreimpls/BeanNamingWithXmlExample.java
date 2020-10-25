b77f08f039f8e6793fd773d9ae14459c4233b275
jacksparrow414 <33389378+jacksparrow414@users.noreply.github.com>
2020-10-25 17:30:42 +0800

    feat(add beanname with @Autowired @Qualifier):

--- src/main/java/com/learn/spring/springuse/basic/examples/beannaming/oneinterfacemoreimpls/BeanNamingWithXmlExample.java
+++ src/main/java/com/learn/spring/springuse/basic/examples/beannaming/oneinterfacemoreimpls/BeanNamingWithXmlExample.java
@@ -1,9 +1,13 @@
 package com.learn.spring.springuse.basic.examples.beannaming.oneinterfacemoreimpls;
 
+import com.learn.spring.springuse.basic.service.beannaming.oneinterfacemoreimpls.BeanNameInterfaceWithXml;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
+ * xml标签<qualifier value="xxx/>结合注解@Qualifier一起使用
+ * 使用见{@link BeanNameInterfaceWithXml}
+ *
  * @author jacksparrow414
  * @date 2020/10/25
  */
--- src/main/java/com/learn/spring/springuse/basic/service/beannaming/oneclassmoreinstances/BeanNameComponent.java
+++ src/main/java/com/learn/spring/springuse/basic/service/beannaming/oneclassmoreinstances/BeanNameComponent.java
@@ -1,10 +1,11 @@
 package com.learn.spring.springuse.basic.service.beannaming.oneclassmoreinstances;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 /**
- * 当注入的类有多个
+ * 当注入的类有多个,同样要指定具体的bean的名字
  *
  * @author jacksparrow414
  * @date 2020/10/25
@@ -13,5 +14,6 @@ import org.springframework.stereotype.Component;
 public class BeanNameComponent {
     
     @Autowired
+    @Qualifier(value = "firstBean")
     private BeanNameService beanNameService;
 }
