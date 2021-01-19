c6cb2f7d919b8cd2400c734bab3bd25a1b74dd65
Serendipity <jacksparrow414@163.com>
2021-01-19 14:56:48 +0800

    加入@Primary使用示例 (#15)

--- src/main/java/com/learn/spring/springuse/basic/service/beannaming/oneinterfacemoreimpls/BeanNameInterfaceComponent.java
+++ src/main/java/com/learn/spring/springuse/basic/service/beannaming/oneinterfacemoreimpls/BeanNameInterfaceComponent.java
@@ -1,13 +1,14 @@
 package com.learn.spring.springuse.basic.service.beannaming.oneinterfacemoreimpls;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 /**
  * 注入属性时，使用Autowired注入
  *
  * 也可以使用@Resource(name ="beanName")进行注入.其中的name为bean的名字
+ *
+ * 还可以使用@Autowired和@Primary一起使用进行注入
  *
  * @author jacksparrow414
  * @date 2020/10/25
@@ -20,9 +21,19 @@ public class BeanNameInterfaceComponent {
      * 必须使用@Qualifier指定其具体的实现的bean的名字.
      * 否则会报 No qualifying bean of type,expected single matching bean but found 2
      */
+//    @Autowired
+//    @Qualifier(value = "First")
+//    private BeanNameService beanNameService;
+
+    /**
+     * 可以直接通过bean的name指定要注入的哪个实现类.
+     */
+//     @Resource(name = "First")
+//     private BeanNameService beanNameService;
+
+    /**
+     * 当一个接口有多个实现类时,还可以采取第三种方法，即在多个实现类中选取一个主要的.
+     */
     @Autowired
-    @Qualifier(value = "First")
-        
-    //@Resource(name = "First")
     private BeanNameService beanNameService;
 }
---
+++ src/main/java/com/learn/spring/springuse/basic/service/beannaming/oneinterfacemoreimpls/ThirdBeanNameServiceImpl.java
@@ -0,0 +1,8 @@
+package com.learn.spring.springuse.basic.service.beannaming.oneinterfacemoreimpls;
+
+/**
+ * @author duhongbo
+ * @date 2021/1/19 11:51
+ */
+public class ThirdBeanNameServiceImpl implements BeanNameService{
+}
