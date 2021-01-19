cbaa5fb205dd3ccb0922b17d3fd37cf2c44c57ab
Serendipity <jacksparrow414@163.com>
2021-01-19 16:08:19 +0800

    加入实现在Bean的生命周期的Bean初始化之后、Bean销毁之前的操作示例(三种方法) (#16)
    * 加入实现在Bean的生命周期的Bean初始化之后、Bean销毁之前的操作示例(三种方法)

---
+++ src/main/java/com/learn/spring/springuse/basic/examples/beanlifecycle/BeanLifeCycleWithCustomMethodByXmlExample.java
@@ -0,0 +1,18 @@
+package com.learn.spring.springuse.basic.examples.beanlifecycle;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 在xml中使用自定义的init-method、destroy-method的示例
+ *
+ * 更详细解释见{@link BeanLifeCycleWithPostAnnotationExample}
+ * @author duhongbo
+ * @date 2021/1/19 15:50
+ */
+public class BeanLifeCycleWithCustomMethodByXmlExample {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("basic/beanlifecycle/BeanLifeCycleWithCustomMethod.xml");
+        applicationContext.close();
+    }
+}
