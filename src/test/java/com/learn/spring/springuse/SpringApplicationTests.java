67c833eacc68f801c407eaab0a9a0b2fd3f22dc7
Serendipity <jacksparrow414@163.com>
2021-02-06 10:38:11 +0800

    增加@ConfigurationProperties单元测试例子 (#19)
    * 增加@ConfigurationProperties单元测试例子

--- src/test/java/com/learn/spring/springuse/SpringApplicationTests.java
+++ src/test/java/com/learn/spring/springuse/SpringApplicationTests.java
@@ -1,11 +1,23 @@
 package com.learn.spring.springuse;
 
+import com.learn.spring.springuse.advanced.conditional.ConditionalOnPropertyConfiguration;
 import org.junit.Test;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
 public final class SpringApplicationTests {
 
+	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner();
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void assertCustomProperties() {
+		contextRunner.withPropertyValues("jack.sparrow.code=3")
+				.withUserConfiguration(ConditionalOnPropertyConfiguration.class).run(context -> {
+					context.getBean("conditionalOnBeanComponent");
+		});
 	}
 
 }
